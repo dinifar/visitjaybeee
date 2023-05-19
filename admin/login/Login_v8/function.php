15ea196886703af6c3eb1a80e7e98db5078528cb
qarinsofiya <78629630+qarinsofiya@users.noreply.github.com>
2023-05-20 02:32:48 +0800

    Sweet Alert
    sweet alert button

--- admin/login/Login_v8/function.php
+++ admin/login/Login_v8/function.php
@@ -10,7 +10,7 @@ $dbname = "visitjaybee";
 
 $conn = mysqli_connect($host, $username, $password, $dbname);
 if (!$conn) {
-	die("Connection failed: " . mysqli_connect_error());
+    die("Connection failed: " . mysqli_connect_error());
 }
 
 // Get the form data
@@ -21,21 +21,23 @@ $password = $_POST["password"];
 $username = mysqli_real_escape_string($conn, $username);
 $password = mysqli_real_escape_string($conn, $password);
 
-
 // Check if the username and password match
 $sql = "SELECT * FROM users WHERE user_name='$username' AND password='$password'";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) == 1) {
-	// Login successful, set session variables and redirect to the home page
-	if ($row['username'] === $username && $row['password'] === $password) 
-            	$_SESSION['username'] = $row['username'];
-            	$_SESSION['id'] = $row['id'];
-            	header("Location: home.php");
-		        exit();
+    // Login successful, set session variables
+    $row = mysqli_fetch_assoc($result);
+    $_SESSION['username'] = $row['user_name'];
+    $_SESSION['id'] = $row['id'];
+
+    // Prepare the response
+    $response = array('success' => true);
+    echo json_encode($response);
 } else {
-	// Login failed, display error message
-	echo "Invalid username or password.";
+    // Login failed, display error message
+    $response = array('success' => false);
+    echo json_encode($response);
 }
 
 // Close the database connection
