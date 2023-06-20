e39e4daf5fbb04588517c36e8182e0a0948e7fc2
qarinsofiya <78629630+qarinsofiya@users.noreply.github.com>
2023-06-20 20:10:04 +0800

    Fix translation page admin

--- admin/translationadmin.php
+++ admin/translationadmin.php
@@ -81,46 +81,8 @@
       </div>
     </section>
 
-<br> 
-			<div class="container">
-				<div class="row justify-content-center">
-					<div class="col-md-7 heading-section text-center ftco-animate">
-						<h2 >Basic Translation</h2>
-					</div>
-				</div>
-					
-					<div class="col-md-12">
-						<div class="wrapper">
-							<br>
-							<div class="dbox w-100 text-center">
-								<div class="text-input">
-								  <textarea spellcheck="false" class="from-text" placeholder="Enter text"></textarea>
-								  <textarea spellcheck="false" readonly disabled class="to-text" placeholder="Translation"></textarea>
-								</div>
-								<ul class="controls">
-								  <li class="row from">
-									<div class="icons">
-									  <i id="from" class="fas fa-volume-up"></i>
-									  <i id="from" class="fas fa-copy"></i>
-									</div>
-									<select></select>
-								  </li>
-								  <li class="exchange"><i class="fas fa-exchange-alt"></i></li>
-								  <li class="row to">
-									<select></select>
-									<div class="icons">
-									  <i id="to" class="fas fa-volume-up"></i>
-									  <i id="to" class="fas fa-copy"></i>
-									</div>
-								  </li>
-								</ul>
-								<button>Translate Text</button>
-							</div>
-							
-						</div>
-					</div>
-				</div>
-			</div>
+<br> <br>
+			
 		
 			<div class="wrapper">
 			<div id="search-container">
@@ -270,7 +232,49 @@
 		</tbody>
 		</table>
 	</div></div>
-<br><br><br>
+<br>
+
+<div class="container">
+				<div class="row justify-content-center">
+					<div class="col-md-7 heading-section text-center ftco-animate">
+						<h2 >Basic Translation</h2>
+					</div>
+				</div>
+					
+					<div class="col-md-12">
+						<div class="wrapper">
+							<br>
+							<div class="dbox w-100 text-center">
+								<div class="text-input">
+								  <textarea spellcheck="false" class="from-text" placeholder="Enter text"></textarea>
+								  <textarea spellcheck="false" readonly disabled class="to-text" placeholder="Translation"></textarea>
+								</div>
+								<ul class="controls">
+								  <li class="row from">
+									<div class="icons">
+									  <i id="from" class="fas fa-volume-up"></i>
+									  <i id="from" class="fas fa-copy"></i>
+									</div>
+									<select></select>
+								  </li>
+								  <li class="exchange"><i class="fas fa-exchange-alt"></i></li>
+								  <li class="row to">
+									<select></select>
+									<div class="icons">
+									  <i id="to" class="fas fa-volume-up"></i>
+									  <i id="to" class="fas fa-copy"></i>
+									</div>
+								  </li>
+								</ul>
+								<button id="buttonTranslate">Translate Text</button>
+							</div>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<br><br>
 
     <footer class="footer">
 			<div class="container">
